79cfe4c83165b881549b1425979ee6424800682a
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2022-09-11 02:38:33 +0200

    already less ugly

--- src/main/java/yt/graven/gravensupport/Main.java
+++ src/main/java/yt/graven/gravensupport/Main.java
@@ -7,34 +7,36 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
 
-@Component
 public class Main {
 
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(BotConfig.class);
 
-        Main main = context.getBean(Main.class);
-        main.run();
+        Startup startup = context.getBean(Startup.class);
+        startup.run();
     }
 
+    @Component
+    private static class Startup {
 
-    private final EventReceiver eventReceiver;
-    private final YamlConfiguration config;
-    private final JDA client;
+        private final EventReceiver eventReceiver;
+        private final YamlConfiguration config;
+        private final JDA client;
 
-    public Main(EventReceiver eventReceiver, YamlConfiguration config, JDA client) {
-        this.eventReceiver = eventReceiver;
-        this.config = config;
-        this.client = client;
-    }
+        public Startup(EventReceiver eventReceiver, YamlConfiguration config, JDA client) {
+            this.eventReceiver = eventReceiver;
+            this.config = config;
+            this.client = client;
+        }
 
-    private void run() {
-        this.client.getPresence().setPresence(
-            Activity.listening(config.getString("config.prefix")
-                + "new | Ouvrez un ticket avec la modération"),
-            false
-        );
+        public void run() {
+            this.client.getPresence().setPresence(
+                Activity.listening(config.getString("config.prefix")
+                    + "new | Ouvrez un ticket avec la modération"),
+                false
+            );
 
-        this.client.addEventListener(eventReceiver);
+            this.client.addEventListener(eventReceiver);
+        }
     }
 }
