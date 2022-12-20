1e94dabbbe6715af6b9202ea7655715bb1f118d3
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2022-12-20 23:54:24 +0100

    better wording

--- src/main/java/yt/graven/gravensupport/common/exception/ImpossibleException.java
+++ src/main/java/yt/graven/gravensupport/common/exception/ImpossibleException.java
@@ -3,7 +3,7 @@ package yt.graven.gravensupport.common.exception;
 /** This exception should only be thrown on an impossible case scenario */
 public class ImpossibleException extends RuntimeException {
 
-  private static final String MESSAGE = "Weird exception: %s";
+  private static final String MESSAGE = "Impossible exception occurred, code needs to be fixed: %s";
 
   public ImpossibleException(String reason) {
     super(String.format(MESSAGE, reason));
