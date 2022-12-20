f3bc9c7ddad16a85e869401a96bea5f642dd718f
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2022-12-20 23:05:02 +0100

    Replace IO by NIO in BotConfig

--- src/main/java/yt/graven/gravensupport/BotConfig.java
+++ src/main/java/yt/graven/gravensupport/BotConfig.java
@@ -1,8 +1,13 @@
 package yt.graven.gravensupport;
 
 import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.EnumSet;
-import java.util.Objects;
+import java.util.Optional;
 import javax.security.auth.login.LoginException;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
@@ -16,35 +21,34 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan("yt.graven.gravensupport")
 public class BotConfig {
   public static final String CONFIGURATION_FILE = "config.yml";
+  public static final String DEFAULT_CONFIG_FILE = "config.default.yml";
 
   // GENERALITIES
 
   @Bean
-  public YamlConfiguration config() throws IOException {
-    File configurationFile = new File(CONFIGURATION_FILE);
-    if (!configurationFile.exists()) {
-      configurationFile.createNewFile();
-      FileWriter writer = new FileWriter(configurationFile);
-      BufferedReader defaultConfig =
-          new BufferedReader(
-              new InputStreamReader(
-                  Objects.requireNonNull(
-                      getClass().getClassLoader().getResourceAsStream("config.default.yml"))));
-      while (defaultConfig.ready()) {
-        writer.write(defaultConfig.readLine());
-        writer.write("\n");
-      }
-      writer.flush();
-      writer.close();
+  public YamlConfiguration config() throws IOException, URISyntaxException {
+    Path configurationFile = Paths.get(CONFIGURATION_FILE);
+
+    if (!Files.exists(configurationFile)) {
+      Files.createFile(configurationFile);
+
+      Path defaultConfigFile = getDefaultConfigFromInsideJar();
+      String defaultConfigurationData = String.join(System.lineSeparator(), Files.readAllLines(defaultConfigFile));
+      Files.writeString(configurationFile, defaultConfigurationData);
 
       throw new RuntimeException("Unable to start bot : Config did not exist !");
     }
 
-    return YamlConfiguration.loadConfiguration(configurationFile);
+    return YamlConfiguration.loadConfiguration(configurationFile.toFile());
+  }
+
+  private Path getDefaultConfigFromInsideJar() throws URISyntaxException {
+    URL resourceUrl = Optional.ofNullable(getClass().getClassLoader().getResource(DEFAULT_CONFIG_FILE)).orElseThrow(RuntimeException::new);
+    return Paths.get(resourceUrl.toURI());
   }
 
   @Bean
-  public JDA jda() throws LoginException, IOException {
+  public JDA jda() throws LoginException, IOException, URISyntaxException {
     return JDABuilder.create(EnumSet.allOf(GatewayIntent.class))
         .setToken(this.config().getString("config.token"))
         .build();
