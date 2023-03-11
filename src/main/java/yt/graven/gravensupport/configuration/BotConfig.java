aca9b7ed745e864dddcef3fe619566c88e493a78
Tom BUTIN <tom.frefre.13@gmail.com>
2023-03-11 18:54:08 +0100

    Fixed project errors

--- src/main/java/yt/graven/gravensupport/configuration/BotConfig.java
+++ src/main/java/yt/graven/gravensupport/configuration/BotConfig.java
@@ -8,7 +8,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.EnumSet;
 import java.util.Optional;
-import javax.security.auth.login.LoginException;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
